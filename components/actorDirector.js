2c8941216453c7f8c432b4e4a82ff7ee694b64a5
bensennah amine <bensennahamine@gmail.com>
2021-07-20 11:14:19 +0200

    finalisation du component actors/director

---
+++ components/actorDirector.js
@@ -0,0 +1,29 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+
+import { Button, Text, Image, View } from "react-native";
+const ActorDirector = (props) => {
+
+    const {title, actors, director, poster} = props;
+
+    const nav = useNavigation();
+
+    const handleBack = () => {
+        nav.goBack();
+    }
+
+    return (
+            <View>
+                <Image source={{uri: `https://image.tmdb.org/t/p/w500/${poster}.jpg`}} />
+            <View>
+                <Text>Titre: {title}</Text>
+                <Text>Realisateur(s): {director}</Text>
+                <Text>Acteurs: {actors}</Text>
+            </View>
+            <Button title='Retour' onPress={handleBack}
+                    color={} />
+        </View>
+    );
+}
+
+export default ActorDirector;
