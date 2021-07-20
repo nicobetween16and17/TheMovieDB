53df1763eb378bef43de2309ef6698a77648d35b
bensennah amine <bensennahamine@gmail.com>
2021-07-20 11:07:48 +0200

    finalisation de la strucure

--- components/item.js
+++ components/item.js
@@ -1,25 +1,38 @@
 import react from 'react'
 import { useNavigation } from 'react-navigation/native'
 import { View, Text, Image, Stylesheet} from 'react-native'
-import globalStyle from '../../global-style'
+import Style from './style'
+
 
 
 Item = (props) => {
     const {
-        movieTitleOriginal, movieTitleFr, genres, releaseYear, synopsis, producers, evaluation, poster
+        movieTitleOriginal, movieTitleFr, genres, releaseYear, synopsis, evaluation, poster, production
     } = props
+
+    const nav = useNavigation();
+
+    const onPressBtn = () => {
+        nav.navigate('acteurRealisateur');
+    };
 
     return (
         
-        <View>
-            <image name='poster' src ={require(poster)}/>
-            <text>{movieTitleOriginal}</text>
-            <text>{movieTitleFr}</text>
-            <text>{genres}</text>
-            <text>{releaseYear}</text>
-            <text>{synopsis}</text>
-            <text>{evaluation}</text>
+        <View style={}>
+        <View style={}>
+                <Image style={} 
+                source={{uri: `https://image.tmdb.org/t/p/w500/${poster}.jpg`}} />
+            <Text style={}>movieTitleOriginal: {movieTitleOriginal}</Text>
+            <Text style={}>Titre fraçais: {movieTitleFr}</Text>
+            <Text style={}>compagnie de production: {production}</Text>
+            <Text style={}>Genre(s): {genres}</Text>
+            <Text style={}>Synopsis: {synopsis}</Text>
+            <Text style={}>Date de sortie: {releaseYear}</Text>
+            
+            <Text style={}>Note: {evaluation}</Text>
         </View>
+        <Button title="Acteurs / Realisateurs" onPress={onPressBtn} accessibilityLabel="Permet de voir le realisateurs et les acteurs de ce film" />
+    </View>
     );
 }
 export default Item
