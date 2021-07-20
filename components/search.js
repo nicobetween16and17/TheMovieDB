153a71eef5758eafe486d1e6ba59669d714eca34
Kibarol <scarfoceb@gmail.com>
2021-07-20 09:10:20 +0200

    search screen / todo : request search via api

--- components/search.js
+++ components/search.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import { Text,ScrollView,View,Button } from 'react-native';
-Search=()=>{
-    return(
-        <>
-         <View>
-            <Text>
-                TODO Search
-            </Text>
-        </View>
-        </>
-    );
-}
+import React from 'react';
+import {Text, View, Button, useState} from 'react-native';
+Search = () => {
+  const [title, settitle] = useState('');
+  const searchMovie = (title) => {
+    // requete
+  };
+  return (
+    <View>
+      <Text>Search a movie :</Text>
+      <TextInput
+        onChangeText={title => settitle(title)}
+        value={userName}
+        placeholder="Movie title"
+      />
+      <Button title="Search movie" onPress={searchMovie(title)} />
+    </View>
+  );
+};
 export default Search;
